11d9f2263400322e152c7241d7c4fe27f2603df1
SarathS95 <sarath9504@gmail.com>
2025-02-07 11:36:21 +1300

    file changes

---
+++ MOdule1/LabExcersies/labExcercise4.js
@@ -0,0 +1,25 @@
+// function for addition
+function add(a, b) {
+    return a + b;
+}
+
+// function for subtraction
+function sub(a, b) {
+    return a - b;
+}
+
+// function for multiplication
+function mul(a, b) {
+    return a * b;
+}
+
+// function for division
+function divide(a, b) {
+    return a / b;
+}
+
+// Call the function
+console.log(sum(10, 20));
+console.log(sub(30, 15));
+console.log(mul(105, 20));
+console.log(divide(100, 10));
