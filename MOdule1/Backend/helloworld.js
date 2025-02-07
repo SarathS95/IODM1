0aff85a914cec259cefa6f4b80fe0dff0ba002ae
SarathS95 <sarath9504@gmail.com>
2025-02-07 13:04:54 +1300

    changes made

--- MOdule1/Backend/helloworld.js
+++ MOdule1/Backend/helloworld.js
@@ -3,4 +3,12 @@ console.log('Hello World!!!!')
 function sayHello() {
     console.log('Hello')
     }
-sayHello()
+sayHello();
+
+function sum(num1, num2, num3) {
+    return num1 + num2;
+}
+
+console.log(sum(5,1));
+console.log(sum(50,40));
+console.log(sum(100,100));
--- MOdule1/LabExcersies/labExcercise4.js
+++ MOdule1/LabExcersies/labExcercise4.js
@@ -18,8 +18,15 @@ function divide(a, b) {
     return a / b;
 }
 
+//function that take name 
+function greeting(firstName, lastName) {
+    console.log(`Hello${firstName} ${lastName}`);
+}
+
+greeting("Sarath", "Satheesan")
+
 // Call the function
-console.log(sum(10, 20));
+console.log(add(10, 20));
 console.log(sub(30, 15));
-console.log(mul(105, 20));
+console.log(multiply(105, 20));
 console.log(divide(100, 10));
