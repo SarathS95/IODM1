52a656ba7e77345aa38030b21a9bb84fce152d93
SarathS95 <sarath9504@gmail.com>
2025-02-07 17:20:53 +1300

    changes made

---
+++ MOdule1/LabExcersies/labExcercise5.js
@@ -0,0 +1,14 @@
+let players = ['Mo Salah', 'Zlatan Ibrahamovic', 'Pep Guardiola', 'Sadio Mane', 'Marcus Rashford'];
+console.log(players); // ['Mo Salah', 'Zlatan Ibrahamovic', 'Pep Guardiola', 'Sadio Mane', 'Marcus Rashford']
+
+players[1] = 'Cristiano Ronaldo'; //['Mo Salah', 'Cristiano Ronaldo', 'Pep Guardiola', 'Sadio Mane', 'Marcus Rashford']
+console.log(players);
+
+players[4] = 'David Beckham'; //['Mo Salah', 'Cristiano Ronaldo', 'Pep Guardiola', 'Sadio Mane', 'David Beckham']
+console.log(players);
+
+players.unshift('Sergio Ramos'); //['Sergio Ramos','Mo Salah', 'Cristiano Ronaldo', 'Pep Guardiola', 'Sadio Mane', 'David Beckham']
+console.log(players);
+
+let lastplayers = players.pop(); //['Sergio Ramos','Mo Salah', 'Cristiano Ronaldo', 'Pep Guardiola', 'Sadio Mane',]
+console.log(players);
