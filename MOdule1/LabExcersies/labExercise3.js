ca765ce98f425f88f05deba7a6792bb2304fbd27
SarathS95 <sarath9504@gmail.com>
2025-02-12 16:46:38 +1300

    Done LabEx3 Mod1

---
+++ MOdule1/LabExcersies/labExercise3.js
@@ -0,0 +1,10 @@
+function colorchange(){
+    document.getElementById("column1").style.backgroundColor ="red";
+    document.getElementById("heading1").style.color ="green";
+    document.getElementById("heading1").innerText = "BlackPanther";
+}
+function colorchange2(){
+    document.getElementById("column2").style.backgroundColor ="green";
+    document.getElementById("heading2").style.color ="orange";
+    document.getElementById("heading2").innerText = "Spider";
+}
