078f6b2df1d15915c8eb920bf1770c4f03281b10
SarathS95 <sarath9504@gmail.com>
2025-03-11 14:17:45 +1300

    comit

--- module5/myapp/index.js
+++ module5/myapp/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
  const app = express()
  const port = 3000
@@ -7,30 +8,37 @@ const express = require('express')
 const corsOptions = {
     origin: "http://127.0.0.1:5500",
 };
-// cors(corsOptions);
-app.use(cors(corsOptions));
-
 // Import routes
 const testRoutes = require("./routes/myTestRoutes")
  const calculatorRoutes = require('./routes/calculatorRoutes');
+ const userRoutes = require('./routes/userRoutes');
+
+
+
+cors(corsOptions);
+app.use(cors(corsOptions));
+
+
  
 
 //  routes
 app.use("/", express.static("public"));
 app.use("/mytest", testRoutes);
 app.use("/calculator",calculatorRoutes );
+app.use('/users', userRoutes);
+
 
  app.listen(port, () => {
   console.log(`Example app listening 
 at http://localhost:${port}`)
  })
 
- app.listen(port1, () => {
-    console.log(`Example app listening 
-  at http://localhost:${port1}`)
-   })
+//  app.listen(port1, () => {
+//     console.log(`Example app listening 
+//   at http://localhost:${port1}`)
+//    })
 
-   app.listen(port2, () => {
-    console.log(`Example app listening
-        at http://localhost:${port2}`)
-   })
+//    app.listen(port2, () => {
+//     console.log(`Example app listening
+//         at http://localhost:${port2}`)
+//    })
