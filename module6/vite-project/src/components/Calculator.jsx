5c11a5acf3a40c7c9c555fdc6e5303e0a2195dd8
SarathS95 <sarath9504@gmail.com>
2025-03-23 16:48:44 +1300

    final

--- module6/vite-project/src/components/Calculator.jsx
+++ module6/vite-project/src/components/Calculator.jsx
@@ -1,33 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function Calculator() {
-    const [num1, setNum1] = useState('')
-    const [num2, setNum2] = useState('')
-    const [operator, setOperator] = useState('')
-    const [outcome, setOutcome] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [operator, setOperator] = useState("");
+  const [outcome, setOutcome] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    let n1 = parseFloat(num1)
-    let n2 = parseFloat(num2)
+    let n1 = parseFloat(num1);
+    let n2 = parseFloat(num2);
 
     if (operator === "+") {
-        setOutcome(n1 +n2);
+      setOutcome(n1 + n2);
     } else if (operator === "-") {
-        setOutcome(n1 - n2)
+      setOutcome(n1 - n2);
     } else if (operator === "*") {
-        setOutcome(n1 * n2)
-    } else if (operator === "/"){
-        setOutcome(n1 /n2)
+      setOutcome(n1 * n2);
+    } else if (operator === "/") {
+      setOutcome(n1 / n2);
     } else {
-        setOutcome("Something Wrong")
+      setOutcome("Something Wrong");
     }
-}
-return (
+  };
+  return (
     <div className="Calculator componentBox">
       <form onSubmit={handleSubmit}>
         <div className="formRow">
-          <label>Number 1:
+          <label>
+            Number 1:
             <input
               type="number"
               value={num1}
@@ -36,17 +37,19 @@ return (
           </label>
         </div>
         <div className="formRow">
-          <label>Operator:
+          <label>
+            Operator:
             <input
               type="text"
               value={operator}
               onChange={(e) => setOperator(e.target.value)}
-              placeholder="+, -, x, ÷"
+              
             />
           </label>
         </div>
         <div className="formRow">
-          <label>Number 2:
+          <label>
+            Number 2:
             <input
               type="number"
               value={num2}
